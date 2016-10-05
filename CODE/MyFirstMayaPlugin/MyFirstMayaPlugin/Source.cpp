2681cb2d1908151f3cc4f96a034a0b44b7b49779
unknown <BTH@H473-0001.students.ad.bth.se>
2016-10-05 17:00:31 +0200

    Added a few callback function.
    Still issues with name change

--- CODE/MyFirstMayaPlugin/MyFirstMayaPlugin/Source.cpp
+++ CODE/MyFirstMayaPlugin/MyFirstMayaPlugin/Source.cpp
@@ -5,6 +5,54 @@
 
 using namespace std;
 
+MCallbackIdArray MyCallbacks;
+
+
+void SetCallbackExistingNodes(MObject& Node, const MString & str, void* clientData)
+{
+	MItDag it(MItDag::kDepthFirst, MFn::kMesh);
+	MGlobal::displayInfo("HereITCOMES!\n\n");
+	while (it.isDone() == false)
+	{
+		MGlobal::displayInfo(it.fullPathName() + "\n\n");
+		it.next();
+	}
+
+}
+
+void NameChangeFunc(MObject& Node, const MString & str, void* clientData)
+{
+	MGlobal::displayInfo("YOUR Favorite Anime Character Says NAMES HAVE BEEN CHANGED");
+}
+
+void InitialFunc(MObject& node, void *clientData)
+{
+	//lights, Textures
+	if (node.hasFn(MFn::kMesh))
+	{	
+		MStatus res;
+		MFnMesh mymesh(node, &res);
+		
+		if (res==MStatus::kSuccess)
+		{
+			MGlobal::displayInfo("\nYES. APPARANTLY THIS IS WHAT A TRUE MESH SHOULD BE\n");
+		}
+		else
+		{
+			MGlobal::displayInfo("\nWHAT!!!THE NODE WAS NOT TRUE MESHIAN");
+		}
+
+		MGlobal::displayInfo("Vocaloid Maya says a Mesh has been created");
+		MCallbackId NameChangedCallback = MNodeMessage::addNameChangedCallback(node, NameChangeFunc, NULL, &res);
+		MyCallbacks.append(NameChangedCallback);
+	}
+
+}
+
+
+
+
+
 // called when the plugin is loaded
 EXPORT MStatus initializePlugin(MObject obj)
 {
@@ -15,8 +63,26 @@ EXPORT MStatus initializePlugin(MObject obj)
 	if (MFAIL(res))  {
 		CHECK_MSTATUS(res);
 	}
+	
+	//make a callback function here.
+
+	MObject Node;
+
+	MCallbackId NodeCreatedCallback = MDGMessage::addNodeAddedCallback(InitialFunc, kDefaultNodeType, NULL, &res);
+	if(res==MStatus::kSuccess)
+	{
+		MGlobal::displayInfo("NodeCallBackCreatedSuccessfully");
+		MyCallbacks.append(NodeCreatedCallback);
+	}
+	else
+	{
+		MGlobal::displayInfo("Callback for Node init unsuccessful");
+	}
+	//MyCallbacks.append(NameChangedCallback);
+	// end of callbacks
 
 	MGlobal::displayInfo("Maya plugin loaded!");
+
 	// if res == kSuccess then the plugin has been loaded,
 	// otherwise is has not.
 	return res;
@@ -30,8 +96,8 @@ EXPORT MStatus uninitializePlugin(MObject obj)
 	MFnPlugin plugin(obj);
 
 	// if any resources have been allocated, release and free here before
-	// returning...
-
+	MMessage::removeCallbacks(MyCallbacks); // returning...
+	
 	MGlobal::displayInfo("Maya plugin unloaded!");
 
 	return MS::kSuccess;
