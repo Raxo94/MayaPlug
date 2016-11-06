c19db570a7db65f5e701333a89fb3b5711d5ce14
unknown <BTH@H473-0009.students.ad.bth.se>
2016-11-06 14:14:48 +0100

    SkickaInVersionen

--- CODE/MyFirstMayaPlugin/MyFirstMayaPlugin/Source.cpp
+++ CODE/MyFirstMayaPlugin/MyFirstMayaPlugin/Source.cpp
@@ -15,21 +15,10 @@ void timerUpdateFunc(float elapsedTime, float lastTime, void* clientData)
 
 }
 
-void SetCallbackExistingNodes(MObject& Node, const MString & str, void* clientData)
-{
-	MItDag it(MItDag::kDepthFirst, MFn::kMesh);
-	MGlobal::displayInfo("HereITCOMES!\n\n");
-	while (it.isDone() == false)
-	{
-		MGlobal::displayInfo(it.fullPathName() + "\n\n");
-		it.next();
-	}
-
-}
 
 void NameChangeFunc(MObject& Node, const MString & str, void* clientData)
 {
-	MGlobal::displayInfo("YOUR Favorite Anime Character Says NAMES HAVE BEEN CHANGED");
+	MGlobal::displayInfo("Mesh Name Has been Changed");
 }
 
 
@@ -37,66 +26,55 @@ void MeshChangedFunc(MNodeMessage::AttributeMessage msg, MPlug & plug, MPlug & o
 {
 	MStatus res;
 	//Hexadecimal, all new numbers are flags & checks against it. | is used to add flags.
-	if (msg & MNodeMessage::kAttributeSet && !plug.isArray() && plug.isElement()) 	
-	{
-		if (res == MStatus::kSuccess)
-		{
-			MObject obj = plug.node(&res);
-			if (res == MStatus::kSuccess)
-			{
-				MFnMesh meshFn(obj, &res);
-				
-					MPoint aPoint;
-					MFnAttribute fnAttr(plug.attribute());
-					fnAttr.type();
-					meshFn.getPoint(plug.logicalIndex(), aPoint, MSpace::kObject);
-					//fnAttr(plug.attribute());
-			}
-		//MGlobal::displayInfo("Vertex Was Moved");
+	if (msg & MNodeMessage::AttributeMessage::kAttributeSet) {
+		//MFnMesh mesh(plug.node(), &res);
+		MGlobal::displayInfo("Mesh Has been changed!");
 
-	
 	}
-	//MPoint vertex;
-	//plug.getValue(vertex);
-	
-
+		
 	
 }
 
 void TransformChangedFunc(MObject& transformNode, MDagMessage::MatrixModifiedFlags& modified, void *clientData)
 {
-	MGlobal::displayInfo("Node is a transform and callback is a success");
+	MGlobal::displayInfo("transform is a a success!");
 }
+
+void topologyChanged(MObject& node, void *clientData)
+{
+	MGlobal::displayInfo("Mesh Topology has been changed!");
+}
+
 void InitialFunc(MObject& node, void *clientData) 
 {
-	//lights, Textures
 	MStatus res;
 	if (node.hasFn(MFn::kMesh))
 	{	
 		
-		
 		MCallbackId NameChangedCallback = MNodeMessage::addNameChangedCallback(node, NameChangeFunc, NULL, &res);
+		if (res = MStatus::kSuccess)
+		{
+			MyCallbacks.append(NameChangedCallback);
+		}
+
 		MCallbackId MeshChangedCallback = MNodeMessage::addAttributeChangedCallback(node, MeshChangedFunc, NULL, &res);
-		MyCallbacks.append(NameChangedCallback);
-		MyCallbacks.append(MeshChangedCallback);
+		if (res = MStatus::kSuccess)
+		{
+			MyCallbacks.append(MeshChangedCallback);
+		}
 
-		//MFnMesh mymesh(MFnTransform(node).child(0), &res); DAVID
-		//MGlobal::displayInfo(mymesh.name());
+		MCallbackId TopologyChangedCallback = MPolyMessage::addPolyTopologyChangedCallback(node, topologyChanged, NULL, &res);
+		if (res = MStatus::kSuccess)
+		{
+			MyCallbacks.append(TopologyChangedCallback);
+		}
 
-
-		//MFnMesh mymesh(node, &res);						CHRISTOFFER
-		//MGlobal::displayInfo(mymesh.name());
-
-		//MGlobal::displayInfo("\nYES. APPARANTLY THIS IS WHAT A TRUE MESH SHOULD BE\n");
-		//MGlobal::displayInfo("Vocaloid Maya says a Mesh has been created");
-		
 	}
 
 	if (node.hasFn(MFn::kTransform))
 	{
 		MFnTransform transformNode(node);
 		MDagPath NodePath = MDagPath::getAPathTo(transformNode.child(0));
-
 
 		MCallbackId TranformChanged = MDagMessage::addWorldMatrixModifiedCallback(NodePath, TransformChangedFunc, NULL, &res);
 		if (res = MStatus::kSuccess)
@@ -110,12 +88,10 @@ void InitialFunc(MObject& node, void *clientData)
 
 
 
-
-// called when the plugin is loaded
 EXPORT MStatus initializePlugin(MObject obj)
 {
-	// most functions will use this variable to indicate for errors
-	MStatus res = MS::kSuccess;
+
+	MStatus res = MS::kSuccess; 	// most functions will use this variable to indicate for errors
 
 	MFnPlugin myPlugin(obj, "Maya plugin", "1.0", "Any", &res);
 	if (MFAIL(res))  {
@@ -127,26 +103,15 @@ EXPORT MStatus initializePlugin(MObject obj)
 	MCallbackId NodeCreatedCallback = MDGMessage::addNodeAddedCallback(InitialFunc, kDefaultNodeType, NULL, &res);
 	if(res==MStatus::kSuccess)
 	{
-		MGlobal::displayInfo("NodeCallBackCreatedSuccessfully");
 		MyCallbacks.append(NodeCreatedCallback);
 	}
-	else
-	{
-		MGlobal::displayInfo("Callback for Node init unsuccessful");
-	}
-
 
 	MCallbackId NodeTimerCallback = MTimerMessage::addTimerCallback(5.f,timerUpdateFunc, NULL, &res);
+	
 	if (res == MStatus::kSuccess)
 	{
-		MGlobal::displayInfo("NodeCallBackCreatedSuccessfully");
 		MyCallbacks.append(NodeTimerCallback);
 	}
-	else
-	{
-		MGlobal::displayInfo("Callback for Node init unsuccessful");
-	}
-	
 
 	MGlobal::displayInfo("Maya plugin loaded!");
 
